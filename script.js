3b69e8624d2e68e8b4798dc1ed4ca6d03475ce28
kencheng <ckk169@gmail.com>
2025-08-27 14:13:04 +0100

    Changed the on flashing rate from two seconds to one, and the off flashing rate from one second to half a second

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 class Heartbeat {
+  constructor() {
+    this._element = document.createElement("div");
+    this._intervalId = null;
+  }
 
-    constructor() {
-        this._element = document.createElement('div');
-        this._intervalId = null;
-    }
-        
-    on() {
-        clearInterval(this._intervalId);
-        this._element.classList.remove('off');
-        this._intervalId = setInterval(() => {
-            this._element.classList.toggle('on');
-        }, 2000);
-    }
+  on() {
+    clearInterval(this._intervalId);
+    this._element.classList.remove("off");
+    this._intervalId = setInterval(() => {
+      this._element.classList.toggle("on");
+    }, 1000);
+  }
 
-    off() {
-        clearInterval(this._intervalId);
-        this._element.classList.remove('on');
-        this._intervalId = setInterval(() => {
-            this._element.classList.toggle('off');
-        }, 1000);
-    }
+  off() {
+    clearInterval(this._intervalId);
+    this._element.classList.remove("on");
+    this._intervalId = setInterval(() => {
+      this._element.classList.toggle("off");
+    }, 500);
+  }
 
-    get element() {
-        return this._element;
-    }
+  get element() {
+    return this._element;
+  }
 }
